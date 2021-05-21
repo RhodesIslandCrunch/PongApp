0329cf811a817b1a2af4cb6dabd14cdd00cbb56d
RhodesIslandCrunch <rhodesislandcrunch@gmail.com>
2021-05-21 13:42:27 -0400

    Chapter 8 Finished

--- app/src/main/java/com/example/pongapp/PongActivity.java
+++ app/src/main/java/com/example/pongapp/PongActivity.java
@@ -1,13 +1,17 @@
 package com.example.pongapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class PongActivity extends AppCompatActivity {
+public class PongActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(getWindow().FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 }
