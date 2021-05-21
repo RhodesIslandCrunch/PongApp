a71040eb3b01ee71514f9368d2a12516d5a27138
RhodesIslandCrunch <rhodesislandcrunch@gmail.com>
2021-05-21 14:46:50 -0400

    Chapter 9 Starting Constructor

--- app/src/main/java/com/example/pongapp/PongActivity.java
+++ app/src/main/java/com/example/pongapp/PongActivity.java
@@ -4,14 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.graphics.Point;
+import android.view.Display;
 
 public class PongActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //variable creation
+        private PongGame mPongGame;
+        //Android stuff, fixing screen etc.
         super.onCreate(savedInstanceState);
+        //Chapter 8
         requestWindowFeature(getWindow().FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        mPongGame = new PongGame(this, size.x, size.y);
+        setContentView(mPongGame);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
     }
 }
--- app/src/main/java/com/example/pongapp/PongGame.java
+++ app/src/main/java/com/example/pongapp/PongGame.java
@@ -1,4 +1,42 @@
 package com.example.pongapp;
 
-public class PongGame {
-}
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class PongGame extends SurfaceView {
+    //are we debugging?
+    private final boolean DEBUGGING = true;
+    //Objects for drawing
+    private SurfaceHolder mOurHolder;
+    private Canvas mCanvas;
+    private Paint mPaint;
+    // FPS
+    private long mFPS;
+    // milliseconds in a second
+    private final int MILLIS_IN_SECOND = 1000;
+    //Screen Resolution
+    private int mScreenX;
+    private int mScreenY;
+    // Text Size
+    private int mFontSize;
+    private int mFontMargin;
+    //other game objects
+    private Bat mBat;
+    private Ball mBall;
+    // score + lives
+    private int mScore;
+    private int mLives;
+};
+//pong game constructor
+// called from pong activity
+//mponggame = new PongGame(this,size.x,size.y)
+/*
+public PongGame(Context context, int x, int y){
+    //Super ... Calls the parent class
+    // constructor of SurfaceView
+    // provided by Android
+    super(context);
+};*/
