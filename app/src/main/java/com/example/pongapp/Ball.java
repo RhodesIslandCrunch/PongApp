0ff571bfa701fe4139cdcf50139bae6345713bfa
RhodesIslandCrunch <rhodesislandcrunch@gmail.com>
2021-05-25 19:31:09 -0400

    Chapter 10 Ball Movement Finished

--- app/src/main/java/com/example/pongapp/Ball.java
+++ app/src/main/java/com/example/pongapp/Ball.java
@@ -36,4 +36,73 @@ public class Ball {
         return mRect;
 
     }
+
+    // Update the ball position.
+    // Called each frame/loop
+
+    void update(long fps){
+        // Move the ball based upon the
+        // horizontal (mXVelocity) and
+        // vertical(mYVelocity) speed
+        // and the current frame rate(fps)
+        // Move the top left corner
+        mRect.left = mRect.left + (mXVelocity / fps);
+        mRect.top = mRect.top + (mYVelocity / fps);
+
+        // Match up the bottom right corner
+        // based on the size of the ball
+        mRect.right = mRect.left + mBallWidth;
+        mRect.bottom = mRect.top + mBallHeight;
+    }
+    // Reverse vertical
+    void reverseYVelocity(){
+        mYVelocity = -mYVelocity;
+    }
+    // Reverse Horizontal
+    void reverseXVelocity(){
+        mXVelocity = -mXVelocity;
+    }
+
+    void reset(int x, int y){
+        // Initialise the four points of the rectangle which defines the ball
+        mRect.left = x / 2;
+        mRect.top = 0;
+        mRect.right = x / 2 + mBallWidth;
+        mRect.bottom = mBallHeight;
+
+        // How fast will the ball travel
+        mYVelocity = -(y / 3);
+        mXVelocity = (x / 2);
+    }
+    void increaseVelocity(){
+        // increase the speed by 10%
+        mXVelocity = mXVelocity * 1.1f;
+        mYVelocity = mYVelocity * 1.1f;
+    }
+    // Bounce the ball back based on
+
+// whether it hits the left or right-hand side
+
+    void batBounce(RectF batPosition){
+
+        // Detect centre of bat
+        float batCenter = batPosition.left +
+                (batPosition.width() / 2);
+        // detect the centre of the ball
+        float ballCenter = mRect.left +
+                (mBallWidth / 2);
+        // Where on the bat did the ball hit?
+        float relativeIntersect = (batCenter - ballCenter);
+        // Pick a bounce direction
+        if(relativeIntersect < 0){
+            // Go right
+            mXVelocity = Math.abs(mXVelocity);
+            // Math.abs = Absolute Value
+        }else{
+            // Go left
+            mXVelocity = -Math.abs(mXVelocity);
+        }
+        // Flip vertical since horizontal is finished
+        reverseYVelocity();
+    }
 }
